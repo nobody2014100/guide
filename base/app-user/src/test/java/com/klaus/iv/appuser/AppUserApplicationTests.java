53ac461acb89660e1009afefb74c5a7f3f1f960a
zhouduqing <zhouduqing@stargraph.cn>
2020-11-24 21:06:51 +0800

    learning jolokia adjust gateway dependencies learn jmx

--- base/app-user/src/test/java/com/klaus/iv/appuser/AppUserApplicationTests.java
+++ base/app-user/src/test/java/com/klaus/iv/appuser/AppUserApplicationTests.java
@@ -1,42 +1,45 @@
 package com.klaus.iv.appuser;
 
+import com.klaus.iv.appuser.controller.UserController;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.test.context.event.annotation.AfterTestMethod;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@SpringBootTest
-@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@Slf4j
+@RunWith(SpringRunner.class)
+@WebMvcTest(UserController.class)
+@AutoConfigureRestDocs(outputDir = "target/snippets")
 class AppUserApplicationTests {
-    @RegisterExtension
-    final RestDocumentationExtension restDocumentation = new RestDocumentationExtension ("custom");
 
     @Autowired
-    private WebApplicationContext context;
     private MockMvc mockMvc;
-
-    @BeforeEach
-    public void setUp(WebApplicationContext webApplicationContext,
-                      RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation))
-                .build();
-    }
 
     @Test
     public void test() throws Exception {
-       this.mockMvc.perform(RestDocumentationRequestBuilders.get("/user/{id}", 1))
+       this.mockMvc.perform(get("/user/{id}", 1).accept(MediaType.APPLICATION_JSON))
+               .andDo(print())
                .andExpect(status().isOk());
    }
 
