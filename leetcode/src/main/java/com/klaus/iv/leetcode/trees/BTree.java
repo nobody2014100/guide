b2a63b2ebd63244d29f071bed8407949bc1fd750
zhouduqing <zhouduqing@stargraph.cn>
2020-12-21 20:25:56 +0800

    practice regx

--- leetcode/src/main/java/com/klaus/iv/leetcode/trees/BTree.java
+++ leetcode/src/main/java/com/klaus/iv/leetcode/trees/BTree.java
@@ -38,6 +38,22 @@ public class BTree {
     }
 
     /**
+     * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+     * 假设一个二叉搜索树具有如下特征：
+     * 节点的左子树只包含小于当前节点的数。
+     * 节点的右子树只包含大于当前节点的数。
+     * 所有左子树和右子树自身必须也是二叉搜索树。
+     * @param root
+     * @return
+     */
+    public static boolean isValidBST(TreeNode root) {
+
+
+
+        return false;
+    }
+
+    /**
      * 树的遍历
      * @param root
      * @param traverseType
